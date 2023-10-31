41d9ffff4b2eb2629e5b97781288c36597dfe613
OphirNaor <98401468+OphirNaor@users.noreply.github.com>
2023-10-31 13:40:23 +0200

    create homePage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,11 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '../globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Bottombar from '../../components/shared/Bottombar';
+import LeftSidebar from '../../components/shared/LeftSidebar';
+import RightSidebar from '../../components/shared/RightSidebar';
+import Topbar from '../../components/shared/Topbar';
+import '../globals.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +22,19 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
