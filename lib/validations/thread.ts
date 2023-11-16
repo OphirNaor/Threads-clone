cf2537e222de592fa441f8ec081b3d011d8a4a4e
OphirNaor <98401468+OphirNaor@users.noreply.github.com>
2023-11-16 14:10:18 +0200

    Update thread.ts

--- lib/validations/thread.ts
+++ lib/validations/thread.ts
@@ -4,3 +4,7 @@ export const ThreadValidation = z.object({
     thread: z.string().min(3, { message: 'Minimum 3 Characters' }),
     accountId: z.string()
 })
+
+export const CommentValidation = z.object({
+    thread: z.string().min(3, { message: 'Minimum 3 Characters' }),
+})
